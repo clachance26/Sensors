918453aa2cd0ef4adea0c1cd99b79b3fa6f8fea5
Christopher <clachance26@yahoo.com>
2016-02-09 22:08:57 -0500

    Added agents and walls

--- core/src/com/mygdx/game/Sensors.java
+++ core/src/com/mygdx/game/Sensors.java
@@ -6,14 +6,43 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Sensors extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+	Texture subject;
+	Texture horizontalWall;
+	Texture verticalWall;
+	Texture agent1;
+	Texture agent2;
+	int a1x, a1y, a2x, a2y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("background.png");
+		subject = new Texture("subject.png");
+		horizontalWall = new Texture("horizontalWall.png");
+		verticalWall = new Texture("verticalWall.png");
+		agent1 = new Texture("agent1.png");
+		agent2 = new Texture("agent2.png");
+
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("agentLocations.txt"));
+			a1x = scanner.nextInt();
+			a1y = scanner.nextInt();
+			a2x = scanner.nextInt();
+			a2y = scanner.nextInt();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -22,6 +51,11 @@ public class Sensors extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background, 0, 0);
+		batch.draw(subject, 800, 100);
+		batch.draw(horizontalWall, 400, 400);
+		batch.draw(verticalWall, 100, 100);
+		batch.draw(agent1, a1x, a1y);
+		batch.draw(agent2, a2x, a2y);
 		batch.end();
 	}
 }
