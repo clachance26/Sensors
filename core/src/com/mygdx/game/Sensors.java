dff0bbcdc6278bda38f16b051b6b94de03eb9199
Christopher <clachance26@yahoo.com>
2016-02-09 21:17:28 -0500

    commit 2

--- core/src/com/mygdx/game/Sensors.java
+++ core/src/com/mygdx/game/Sensors.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Sensors extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
 	}
 
 	@Override
@@ -21,7 +21,7 @@ public class Sensors extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
 		batch.end();
 	}
 }
