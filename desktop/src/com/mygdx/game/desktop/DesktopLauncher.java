e1428b05bdbd1c34333c4404483ad8ac2781cacf
Christopher <clachance26@yahoo.com>
2016-02-09 21:21:01 -0500

    Window size set

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,5 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Sensors(), config);
+		config.width = 900;
+		config.height = 600;
 	}
 }
